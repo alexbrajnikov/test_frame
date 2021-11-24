b8ff90c5bf977b812623ab6b58a2c0620d1d8bed
alexbrajnikov <93008381+alexbrajnikov@users.noreply.github.com>
2021-11-24 10:11:27 -0500

    Update logfile.py

--- logfile.py
+++ logfile.py
@@ -2,7 +2,7 @@ import logging
 
 def get_logger():
     loggerName = inspect.stack()[1][3]
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(loggerName)
     fileHandler = logging.FileHandler('logfile.log')
     formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s : %(message)s")
     fileHandler.setFormatter(formatter)
