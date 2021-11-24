f2e3369c1494a61d5cc0d29db267b7b4d45405d3
alexbrajnikov <93008381+alexbrajnikov@users.noreply.github.com>
2021-11-24 09:57:55 -0500

    Create logfile.py

---
+++ logfile.py
@@ -0,0 +1,10 @@
+import logging
+
+def test_logging():
+    
+    logger = logging.getLogger(__name__)
+    fileHandler = logging.FileHandler('logfile.log')
+    formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s : %(message)s")
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler)
+    logger.setLevel(logging.INFO)
